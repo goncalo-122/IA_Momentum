42e47b81a0a5213172d2b867e088885959ce8231
Vasco Santos <silvavascosantos@gmail.com>
2024-10-25 11:31:18 +0100

    first commit

---
+++ src/upt/jt/ia/GameMomentumAB.java
@@ -0,0 +1,82 @@
+package upt.jt.ia;
+
+import java.util.ArrayList;
+
+public class GameMomentumAB extends upt.jt.ia.NodeGameAB {
+
+	private int[][] board = new int[7][7];
+	private int myColor;
+
+	public GameMomentumAB(String node) {
+		super(1);
+		myColor = getPlayer();
+		processNode(node);
+	}
+
+	public GameMomentumAB(int[][] p, int myColor, int depth) {
+		super(depth);
+		for (int l = 0; l < 7; l++)
+			for (int c = 0; c < 7; c++)
+				this.board[l][c] = p[l][c];
+		this.myColor = myColor;
+	}
+
+	public void processNode(String node) {
+		String[] v = node.trim().split(" ");
+		for (int l = 0; l < 7; l++)
+			for (int c = 0; c < 7; c++)
+				try {
+					board[l][c] = Integer.parseInt(v[l * 7 + c]);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					System.out.println("board " + v + "  l " + l + "  c " + c);
+				}
+	}
+
+	public ArrayList<upt.jt.ia.Move> expandAB() {
+		ArrayList<upt.jt.ia.Move> suc = new ArrayList<upt.jt.ia.Move>();
+		// TODO
+		return suc;
+	}
+
+	public double getH() {
+		double h = 0;
+		// TODO
+		return h;
+	}
+
+	private int[][] makeCopy(int[][] p) {
+		int[][] np = new int[7][7];
+		for (int l = 0; l < 7; l++)
+			for (int c = 0; c < 7; c++)
+				np[l][c] = p[l][c];
+		return np;
+	}
+
+	public void setMyColor(int color) {
+		myColor = color;
+	}
+
+	public String toString() {
+		String st = "";
+		for (int l = 0; l < 7; l++) {
+			for (int c = 0; c < 7; c++) {
+				st += " " + (board[l][c] == 0 ? "." : "" + (board[l][c]));
+			}
+			st += "\n";
+		}
+		st += "\n";
+		return st;
+	}
+
+	// for testing
+	public static void main(String[] args) {
+		GameMomentumAB jogo = new GameMomentumAB("0 0 0 0 0 0 0 " + "0 0 1 0 0 0 0 " + "0 0 1 0 0 0 0 "
+				+ "0 0 0 2 2 2 2 " + "0 0 0 0 0 0 0 " + "0 0 0 0 0 0 0 " + "0 0 0 0 0 0 0 ");
+		jogo.setMyColor(1);
+		ArrayList<upt.jt.ia.Move> suc = jogo.expandAB();
+		for (upt.jt.ia.Move j : suc)
+			System.out.println(j);
+	}
+
+}
---
+++ src/upt/jt/ia/Move.java
@@ -0,0 +1,27 @@
+package upt.jt.ia;
+
+
+public class Move
+{
+    private String action;
+    private NodeGameAB node;
+    
+    public Move( String action, NodeGameAB node)
+    {
+        this.action = action;
+        this.node = node;
+    }
+
+    public String getAction() {
+        return action;
+    }
+    
+    public NodeGameAB getNode() {
+        return node;
+    }
+    
+    public String toString() {
+        return "Move: "+ action+"\n"+node.toString();
+    }
+
+}
